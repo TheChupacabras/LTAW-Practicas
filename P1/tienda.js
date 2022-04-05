be6e8f673813fc423ccff7f96566fe1af3f5a71e
TheChupacabras <a.delgadom.2016@alumnos.urjc.es>
2022-04-05 13:24:44 +0200

    primera parte

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,75 +1,72 @@
-//-- Ejemplo 7. MODULO HTTP
-//-- Servidor que devuelve una página en HTML cuando se le pide
-//-- el recurso raiz (/), y devuelve una página de ERROR también
-//-- en HTML cuando se pide cualquier otro recurso
-
+// modules
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
 
 const PUERTO = 9090;
 
-//-- Texto HTML de la página principal
-const pagina_main = `
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi tienda</title>
-</head>
-<body style="background-color: lightblue">
-    <h1 style="color: green">MI TIENDA</h1>
-</body>
-</html>
-`
+// mine utilizado
+var mine = {
+        '/'    : 'text/html',
+        'html' : 'text/html',
+        'css'  : 'text/css',
+        'jpg'  : 'image/jpg',
+        'js'   : 'text/js',
+      
+    };
 
-//-- Texto HTML de la página de error
-const pagina_error = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi tienda</title>
-</head>
-<body style="background-color: red">
-    <h1 style="color: white">ERROR!!!!</h1>
-</body>
-</html>
-`
 
-const server = http.createServer((req, res)=>{
-    console.log("Petición recibida!");
+//-- Servidor escuchando
+console.log("Server is listening");
 
-    //-- Valores de la respuesta por defecto
-    let code = 200;
-    let code_msg = "OK";
-    let page = pagina_main;
 
-    //-- Analizar el recurso
-    //-- Construir el objeto url con la url de la solicitud
-    const url = new URL(req.url, 'http://' + req.headers['host']);
-    console.log(url.pathname);
+//-- Server
+const server = http.createServer(function (req, res) {
+    
+    //url solicitada
+    let filename = ""
+    const myUrl = new URL(req.url, 'http://' + req.headers['host']);
+    console.log("\nSe ha solicitado el recurso: " + myUrl.pathname);
 
-    //-- Cualquier recurso que no sea la página principal
-    //-- genera un error
-    if (url.pathname != '/') {
-        code = 404;
-        code_msg = "Not Found";
-        page = pagina_error;
+    
+    console.log("Peticion Recibida: " + myUrl);
+
+
+    //buscamos archivo
+    if(myUrl.pathname == '/'){
+        //inicio
+        filename = "./tienda.html";
+    }else{
+        filename = "." + myUrl.pathname;
     }
+    
+    console.log("Filename:",filename);
 
-    //-- Generar la respusta en función de las variables
-    //-- code, code_msg y page
-    res.statusCode = code;
-    res.statusMessage = code_msg;
-    res.setHeader('Content-Type','text/html');
-    res.write(page);
-    res.end();
+
+    content = myUrl.pathname.split(["."])[1];
+    content_type = myUrl.pathname.slice(content);
+    console.log("Tipo de mine:", mine[content_type])
+
+
+    fs.readFile(filename, function(err, data){
+
+        //si hay error
+        if (err) {           
+            data = fs.readFileSync('./error.html')
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+            
+        }else{
+            res.writeHead(200, {'Content-Type': mine[content_type]});
+            res.write(data);
+            res.end();
+        }
+    });
 });
 
 server.listen(PUERTO);
 
-console.log("Ejemplo 7. Escuchando en puerto: " + PUERTO);
+console.log("La pagina está activa. Escuchando en puerto: " + PUERTO);
